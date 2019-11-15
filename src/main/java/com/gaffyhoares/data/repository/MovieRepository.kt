f7aa24a6fe223ec8889d16935dbbc20cc2533f51
Gaffy Akthar <gaffy.akthar@futureplatforms.com>
2019-11-15 11:56:10 +0000

    hoaresbank - creating movie info activity and item layout for recyclerview and movie repository

---
+++ src/main/java/com/gaffyhoares/data/repository/MovieRepository.kt
@@ -0,0 +1,24 @@
+package com.gaffyhoares.data.repository
+
+import androidx.lifecycle.MutableLiveData
+import com.gaffyhoares.data.network.ApiUtils
+import com.gaffyhoares.data.network.response.MoviesResult
+import com.gaffyhoares.utils.safeApi
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+
+
+class MovieRepository {
+
+    private val mutableLiveData = MutableLiveData<List<MoviesResult>>()
+
+    fun getMutableLiveData(): MutableLiveData<List<MoviesResult>> {
+        GlobalScope.launch {
+            val api = safeApi { ApiUtils.getMyApi().getTrendingMovies() }
+            if (api.results != null) {
+                mutableLiveData.postValue(api.results)
+            }
+        }
+        return mutableLiveData
+    }
+}
